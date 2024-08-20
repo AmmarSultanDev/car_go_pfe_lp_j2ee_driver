6adfa00623c98d4a25fbf7274ae4acdb1cac6627
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-20 19:56:54 +0100

    chore: Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,3 @@
+# car_go_pfe_lp_j2ee_driver
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/car_go_pfe_lp_j2ee_driver/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.car_go_pfe_lp_j2ee_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
